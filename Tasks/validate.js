587c7133256ec331ff5c2213b23adfbf6e85d2f1
Illia Dymura <illiadimura@gmail.com>
2025-01-13 16:45:54 +0200

    feat: validate.js: add comment

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,6 +1,10 @@
 'use strict';
 
-// Validates person name
-const isValidPersonName = (name) => /^[a-zA-Z]+( [a-zA-Z]+)+$/.test(name);
+// Valid person name
+// 	1.	Only english alphabetic characters.
+// 	2.	At least two words.
+// 	3.	Words are separated with a single space.
+const validPersonNamePattern = /^[a-zA-Z]+( [a-zA-Z]+)+$/;
+const isValidPersonName = (name) => validPersonNamePattern.test(name);
 
 module.exports = isValidPersonName;
